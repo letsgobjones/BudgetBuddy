cfb393a493d5c7e68d67320dbb75f9c6eab4207e
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-08-29 16:42:00 +0900

    Created BudgetCategory Model and Extensions.

---
+++ BudgetApp/Models/Extensions/BudgetCategory+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetCategory+CoreDataClass.swift
+//  BudgetApp
+//
+//  Created by Brandon Jones on 8/29/24.
+//
+
+import Foundation
+import CoreData
+
+@objc(BudgetCategory)
+public class BudgetCategory: NSManagedObject {
+    
+  public override func awakeFromInsert() {
+    self.dateCreated = Date()
+  }
+}
