8ef289373290c0ec2ad5e159200ab2eef5a5ed6c
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-08-29 16:26:35 +0900

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Brandon Jones on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
