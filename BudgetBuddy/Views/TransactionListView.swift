a4fb50084ec263f2884877f046a20b99e3ccb1cd
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-09-08 21:45:15 +0900

    Created TransactionListView

---
+++ BudgetBuddy/Views/TransactionListView.swift
@@ -0,0 +1,42 @@
+//
+//  TransactionListView.swift
+//  BudgetBuddy
+//
+//  Created by Brandon Jones on 9/8/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct TransactionListView: View {
+  
+  @FetchRequest var transactions: FetchedResults<Transaction>
+  
+  init(request: NSFetchRequest<Transaction>) {
+    _transactions = FetchRequest(fetchRequest: request)
+    
+  }
+  
+  
+    var body: some View {
+      if transactions.isEmpty {
+        Text("No Transactions,")
+      } else {
+        List {
+          ForEach(transactions) { transaction in
+            
+            HStack {
+              
+              Text(transaction.title ?? "")
+              Spacer()
+              Text(transaction.total, format: .currency(code: "USD"))
+            }
+            
+          }
+        }
+//        .clipShape(RoundedRectangle(cornerRadius: 10))
+        
+      }
+    }
+}
+
